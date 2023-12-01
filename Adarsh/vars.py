d1aaf1b5b7b284d8e9ff778367e46d318c950c2b
geetesh07 <70388959+geetesh07@users.noreply.github.com>
2023-12-01 07:20:13 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -10,12 +10,12 @@ load_dotenv()
 class Var(object):
     MULTI_CLIENT = False
     API_ID = int(getenv('API_ID', '6572614'))
-    API_HASH = str(getenv('API_HASH', 'AAGbHvEAqZhyAeweaZiFFhw4dOd0Oq_blFI'))
-    BOT_TOKEN = str(getenv('BOT_TOKEN', ''))
+    API_HASH = str(getenv('API_HASH', 'a0df85f9cdca64fa7418d41ace4b6513'))
+    BOT_TOKEN = str(getenv('BOT_TOKEN', '6850555173:AAGbHvEAqZhyAeweaZiFFhw4dOd0Oq_blFI'))
     name = str(getenv('name', 'filetolinkggbot'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1002094600942'))
+    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '1703911083'))
     PORT = int(getenv('PORT', 8080))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
