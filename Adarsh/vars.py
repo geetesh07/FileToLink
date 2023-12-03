df2a4ab2783ddc0bd79947465984ed3e5a6698fe
geetesh07 <70388959+geetesh07@users.noreply.github.com>
2023-12-03 11:35:15 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -15,7 +15,7 @@ class Var(object):
     name = str(getenv('name', 'filetolinkggbot'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1002107248021'))
+    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1002062515857'))
     PORT = int(getenv('PORT', 8080))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
@@ -37,4 +37,4 @@ class Var(object):
         URL = "http://{}/".format(FQDN)
     DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://MONGOO:MONGOO@cluster0.kquc5py.mongodb.net/?retryWrites=true&w=majority'))
     UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', 'Channel'))
-    BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001362659779")).split())) 
+    BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "")).split())) 
